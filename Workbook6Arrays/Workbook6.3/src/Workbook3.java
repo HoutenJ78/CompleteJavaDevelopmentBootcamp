c7216050a7f5e8926e41e6265bb0cf0f3e846d7d
HoutenJ78 <104566296+HoutenJ78@users.noreply.github.com>
2022-04-29 11:24:16 +0200

    Add files via upload

---
+++ Workbook6Arrays/Workbook6.3/src/Workbook3.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Workbook3 {
+    public static void main(String[] args) {
+
+        /* Task 1: Create an array that stores each aisle:
+             apples – bananas – candy – chocolate – coffee – tea
+
+             Then, use a for loop to print each element in the array.
+        */
+        String [] aisle = new String[] {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
+        System.out.println(Arrays.toString(aisle));
+
+        /* Task 2:
+           1. System.out.println("\nDo you sell coffee?");
+
+           2. Using the for loop from task 1:
+            • check if Java Grocer sells coffee.
+            • if so, break the loop and print: \nWe have that in aisle: <index>
+        */
+        System.out.println("\nDo you sell coffee?");
+        for (int i = 0; i < aisle.length; i++) {
+            if (aisle[i].equals("coffee")) {
+                System.out.println("\nWe have that in aisle: " + i);
+                break;
+            }
+        }
+    }
+}
